ee1e85f9ff423dab0ec4c1f387d05ac42d76ba05
Vishesh Kumar <vk7879@srmist.edu.in>
2022-02-26 17:54:06 +0530

    Update services.dart

--- lib/Screens/services.dart
+++ lib/Screens/services.dart
@@ -18,138 +18,70 @@ class _ServicesState extends State<Services> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Padding(
+        resizeToAvoidBottomInset: false,
+        body: Padding(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          //mainAxisAlignment: MainAxisAlignment.,
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    //mainAxisAlignment: MainAxisAlignment.,
 
-          children: <Widget>[
-            SizedBox(height: 55),
-            Container(child: Image.asset('image/logo.png')),
-            SizedBox(height: 30),
-            Container(
-              child: Text(
-                'Services Around Me',
-                style: GoogleFonts.poppins(
-                    //color : Color(0xff57559E),
-                    color: Color(0xff2F303A),
-                    fontWeight: FontWeight.w500,
-                    fontSize: 25),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadiusDirectional.circular(5)),
-              child: TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xffECF0F5),
-                    border: InputBorder.none,
-                    labelText :'Search Organisation...',
-                    prefixIcon: Icon(CupertinoIcons.search,color: Colors.black,)
-                ),
-              ),
-            ),
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // crossAxisAlignment: CrossAxisAlignment.s,
-            //children: [
-            SizedBox(height: 20,),
-            Card(color: Colors.teal,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          'Hospital',
-                          style:
-                          TextStyle(color: Colors.white, fontSize: 20, height: 1,fontWeight: FontWeight.w700),
-                        ),
+    children: <Widget>[
+    SizedBox(height: 55),
+    Container(child: Image.asset('image/logo.png')),
+    SizedBox(height: 30),
+    Container(
+    child: Text(
+    'Services Around Me',
+    style: GoogleFonts.poppins(
+    //color : Color(0xff57559E),
+    color: Color(0xff2F303A),
+    fontWeight: FontWeight.w500,
+    fontSize: 25),
+    ),
+    ),
+    SizedBox(
+    height: 10,
+    ),
+    Container(
+    decoration: BoxDecoration(
+    borderRadius: BorderRadiusDirectional.circular(5)),
+    child: TextFormField(
+    obscureText: true,
+    decoration: InputDecoration(
+    filled: true,
+    fillColor: Color(0xffECF0F5),
+    border: InputBorder.none,
+    labelText :'Search Organisation...',
+    prefixIcon: Icon(CupertinoIcons.search,color: Colors.black,)
+    ),
+    ),
+    ),
+    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    // crossAxisAlignment: CrossAxisAlignment.s,
+    //children: [
+    SizedBox(height: 20,),
+    Card(color: Colors.teal,shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(15.0)),
+    child: Container(
+    height: MediaQuery.of(context).size.height*0.1,
+    width: MediaQuery.of(context).size.width*0.9,
+    child: Column(
+    children: [
+    Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: [
+    Text(
+    'Hospital',
+    style:
+    TextStyle(color: Colors.white, fontSize: 20, height: 1,fontWeight: FontWeight.w700),
+    ),
 
-                        Icon(Icons.phone_outlined,color: Colors.white,)
-                      ],
-                    ),
-                    Text('lorem epsum',style: GoogleFonts.poppins(),)
-                    ,Text('+91 987654321')
+    Icon(Icons.phone_outlined,color: Colors.white,)
+    ],
+    ),
+    Text('lorem epsum',style: GoogleFonts.poppins(),)
+    ,Text('+91 987654321'),
 
-                  ],
-                )),
-              SizedBox(
-                width: MediaQuery.of(context).size.width*0.1,height: MediaQuery.of(context).size.height*0.1,
-              ),
-              Card(color: Colors.orange,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text(
-                            'NGO',
-                            style:
-                            TextStyle(color: Colors.white, fontSize: 20, height: 1,fontWeight: FontWeight.w700),
-                          ),
-
-                          Icon(Icons.phone_outlined,color: Colors.white,)
-                        ],
-                      ),
-                      Text('lorem epsum',style: GoogleFonts.poppins(),)
-                      ,Text('+91 987654321')
-                    ],
-                  )),
-        SizedBox(
-          width: MediaQuery.of(context).size.width*0.1,height: MediaQuery.of(context).size.height*0.1,),
-            Card(color: Colors.purpleAccent,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          'Police',
-                          style:
-                          TextStyle(color: Colors.white, fontSize: 20, height: 1,fontWeight: FontWeight.w700),
-                        ),
-
-                        Icon(Icons.phone_outlined,color: Colors.white,)
-                      ],
-                    ),
-                    Text('lorem epsum',style: GoogleFonts.poppins(),)
-                    ,Text('+91 987654321')
-                  ],
-                )),
-            SizedBox(
-              width: MediaQuery.of(context).size.width*0.1,height: MediaQuery.of(context).size.height*0.1,),
-            Card(color: Colors.redAccent,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          'Fire Department',
-                          style:
-                          TextStyle(color: Colors.white, fontSize: 20, height: 1,fontWeight: FontWeight.w700),
-                        ),
-
-                        Icon(Icons.phone_outlined,color: Colors.white,)
-                      ],
-                    ),
-                    Text('lorem epsum',style: GoogleFonts.poppins(),)
-                    ,Text('+91 987654321')
-                  ],
-                )),
-
-
-          ],
-        ),
-
-      ),
-    );
-  }
+    )));
+    }
 }
